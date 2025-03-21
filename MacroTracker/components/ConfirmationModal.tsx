1b1d0a06784c70bb4845486a43a692bbe160b1a5
danprav04 <62667601+danprav04@users.noreply.github.com>
2025-03-21 18:27:45 +0200

    fixed confirmation styles

--- MacroTracker/components/ConfirmationModal.tsx
+++ MacroTracker/components/ConfirmationModal.tsx
@@ -25,12 +25,18 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
   const { theme } = useTheme();
 
   return (
-    <Overlay isVisible={isVisible} onBackdropPress={onCancel} overlayStyle={styles.overlay}>
-      <View style={styles.container}>
+    <Overlay
+      isVisible={isVisible}
+      onBackdropPress={onCancel}
+      overlayStyle={styles.overlay}
+      backdropStyle={styles.backdrop} // Add the backdropStyle
+    >
+      <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
         <Text style={[styles.title, {color: theme.colors.text}]}>{title}</Text>
         <Text style={[styles.message, {color: theme.colors.text}]}>{message}</Text>
         <Input
           placeholder="Enter confirmation text"
+          placeholderTextColor={theme.colors.text}
           value={confirmationText}
           onChangeText={setConfirmationText}
           containerStyle={styles.inputContainer}
@@ -56,9 +62,12 @@ const styles = StyleSheet.create({
     overlay: {
         borderRadius: 10,
         width: '80%', // Responsive width
+        padding: 6,
+        backgroundColor: 'rgba(200, 200, 200, 0.5)'
     },
   container: {
     padding: 20,
+    borderRadius: 7,
   },
   title: {
     fontSize: 18,
@@ -80,6 +89,9 @@ const styles = StyleSheet.create({
   button: {
     width: 100,
     padding: 10,
+  },
+  backdrop: {  // Add styles for the backdrop
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black
   },
 });
 
