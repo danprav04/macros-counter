fbc16a6ee1774aa8745c09fc273488fb341dec52
Daniel Pravutiner <62667601+danprav04@users.noreply.github.com>
2025-08-01 03:09:36 +0300

    Fixed types for tests

--- MacroTracker/jest.config.js
+++ MacroTracker/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
-  setupFilesAfterEnv: ['./jest-setup.js'],
+  setupFilesAfterEnv: ['./jest-setup.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui/.*)',
   ],
