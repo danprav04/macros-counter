00259abeee283c8783400268ac37b9fbc967498b
Daniel Pravutiner <62667601+danprav04@users.noreply.github.com>
2026-02-20 00:26:30 +0200

    Revert "dev"
    This reverts commit 2ccfd2c5444a86a7bf66c6f4185e60e6699c9909.

--- MacroTracker/app.config.js
+++ MacroTracker/app.config.js
@@ -1,5 +1,5 @@
 export default {
-  name: "DEV Macros Vision AI",
+  name: "Macros Vision AI",
   slug: "MacroTracker",
   version: "4.4.1",
   runtimeVersion: {
@@ -43,7 +43,7 @@ export default {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#ffffff"
     },
-    package: "com.macrosvisionai.app.dev",
+    package: "com.macrosvisionai.app",
     versionCode: 22,
     userInterfaceStyle: "automatic",
     permissions: [
