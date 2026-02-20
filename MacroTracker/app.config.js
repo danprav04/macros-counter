619d470d2c8cba2ce027fbfe3e5280da547b3d9b
Daniel Pravutiner <62667601+danprav04@users.noreply.github.com>
2026-02-20 21:24:26 +0200

    dev

--- MacroTracker/app.config.js
+++ MacroTracker/app.config.js
@@ -1,5 +1,5 @@
 export default {
-  name: "Macros Vision AI",
+  name: "DEV Macros Vision AI",
   slug: "MacroTracker",
   version: "4.5.0",
   runtimeVersion: {
@@ -41,7 +41,7 @@ export default {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#ffffff"
     },
-    package: "com.macrosvisionai.app",
+    package: "com.macrosvisionai.app.dev",
     versionCode: 22,
     userInterfaceStyle: "automatic",
     permissions: [
